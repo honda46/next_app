8aa4f4a74ed5ed7d65c75d77b719404cd15f5545
Ken Takahashi <takaken@redimpulz.com>
2020-12-04 16:45:34 +0900

    gitで管理しないファイルを削除

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/fire":["static\u002Fchunks\u002Fpages\u002Ffire.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ffire"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- components/QR_sample.js
+++ components/QR_sample.js
@@ -1,26 +1,25 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 import firebase from "firebase";
 import "firebase/storage";
-import { useQRCode } from 'react-hook-qrcode';
-import Link from 'next/link';
+import { useQRCode } from "react-hook-qrcode";
+import Link from "next/link";
 import QRCode from "qrcode.react";
 
 class QR_sample extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      data: "a"
-    }
+      data: "a",
+    };
   }
 
-  async getFireData(){
+  async getFireData() {
     let Result = "Test";
     const db = firebase.firestore();
-    const doc = await db.collection('users').doc('001').get();
-    Result = JSON.stringify(doc.data(),undefined,1);
-    console.log("Document data:",Result);//ここのResultにはちゃんと格納されている
-    this.setState(state => ({
+    const doc = await db.collection("users").doc("001").get();
+    Result = JSON.stringify(doc.data(), undefined, 1);
+    console.log("Document data:", Result); //ここのResultにはちゃんと格納されている
+    this.setState((state) => ({
       data: Result,
     }));
     //return Result;
@@ -33,24 +32,22 @@ class QR_sample extends Component {
   //   //コンソールの順番が getQr → getFireDataになっている
   // }
 
-
-  render(){
-     while(this.state.data == "a"){
-       this.getFireData();
-     }
+  render() {
+    while (this.state.data == "a") {
+      this.getFireData();
+    }
     return (
       <div>
-          <button>取得</button>
-          <div>
-            <QRCode value= {this.state.data} />
-          </div>
-          <Link href="/other">
-            <a>Back to Home</a>
-          </Link>
+        <button>取得</button>
+        <div>
+          <QRCode value={this.state.data} />
+        </div>
+        <Link href="/other">
+          <a>Back to Home</a>
+        </Link>
       </div>
     );
   }
-
 }
 export default QR_sample;
 
